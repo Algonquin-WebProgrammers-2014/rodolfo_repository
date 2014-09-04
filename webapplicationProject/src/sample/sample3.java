68f9d532d4f9ec103ef12aa4c2f00575d00a9f72
poldz123 <rodolfo_navalon@yahoo.com>
2014-09-04 16:59:24 -0400

    Second change

--- webapplicationProject/src/sample/sample3.java
+++ webapplicationProject/src/sample/sample3.java
@@ -3,4 +3,7 @@ package sample;
 public abstract class sample3 {
 
 	public abstract void render();
+	public abstract void render2();
+	public abstract void render3();
+	public abstract void render4();
 }
