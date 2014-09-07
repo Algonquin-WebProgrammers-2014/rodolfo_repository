296dcd3e7ead2bb087d23c8d581113ee0f9a5de1
poldz123 <rodolfo_navalon@yahoo.com>
2014-09-07 03:43:10 -0400

    New Change from project

--- webapplicationProject/src/sample/sample2.java
+++ webapplicationProject/src/sample/sample2.java
@@ -7,4 +7,28 @@ public class sample2 extends sample3{
 		
 	}
 
+	@Override
+	public void render2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render5() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
